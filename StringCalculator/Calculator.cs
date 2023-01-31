a37f168a271d3c832dee3d1f4b945121afa2c368
Artur Danilov <artur.danilov@adesso.de>
2023-01-31 10:08:51 +0100

    refactoring add +, -, *, /

--- StringCalculator/Calculator.cs
+++ StringCalculator/Calculator.cs
@@ -8,42 +8,54 @@ namespace StringCalculator
 {
     public class Calculator
     {
-        public int AddAndParseNumber(string input)
+        public int Add(string input)
         {
+            //if 0 or empty
             if (input == "" || input == "0")
                 return 0;
+
+            string[] number = input.Split(' ');
+
+            //if one parameter || is integer
+            int intCount = 0;
+            foreach (string s in number)
+                intCount++;
+            if (intCount == 1)
+                return int.Parse(input);
+
+            //if two parameter wilth delimeter
             
-            char delimeter;
+            var inputSplit = input.Split(
+                new char[] { ' ', ',' },
+                StringSplitOptions.RemoveEmptyEntries);
 
-            //if delimeter = '+' => +;
-            //if delimeter = '-' => -;
-            //if delimeter = '*' => *;
-            //if delimeter = '/' => /;
-
+            //take delimeter
+            char delimeter = ' ';
             if (input.Contains('+'))
-            {
                 delimeter = '+';
-            }
             if (input.Contains('-'))
-            {
                 delimeter = '-';
-            }
             if (input.Contains('*'))
-            {
                 delimeter = '*';
-            }
-            else
+            if (input.Contains('/'))
+                delimeter = '/';
+
+            int a = int.Parse(inputSplit[0]);
+            int b = int.Parse(inputSplit[2]);
+            
+            switch (delimeter)
             {
-                delimeter= '/';
+                case '+':
+                    return a + b;
+                case '-':
+                    return a - b;
+                case '*':
+                    return a * b;
+                case '/':
+                    return a / b;
+                default:
+                    throw new ArgumentException("Invalid operator");
             }
-
-            var inputSplit = input.Split(',');
-            int sum = 0;
-
-            foreach (var i in inputSplit)
-                sum += int.Parse(i);
-
-            return sum;
         }
     }
 }
--- StringCalculatorTests/AddNumbersTests.cs
+++ StringCalculatorTests/AddNumbersTests.cs
@@ -4,17 +4,23 @@ namespace StringCalculatorTests
 {
     public class AddNumbersTests
     {
+
+        Calculator _calculator = new Calculator();
         [SetUp]
         public void Setup()
         {
+            _calculator = new Calculator();
         }
-
-        Calculator _calculator = new Calculator();
+        [TearDown]
+        public void TearDown()
+        {
+            _calculator = null;
+        }
 
         [Test]
         public void AddNumbers_InputEmpty_Return0()
         {
-            var actual = _calculator.AddAndParseNumber("");
+            var actual = _calculator.Add("");
 
             Assert.That(actual, Is.EqualTo(0));
         }
@@ -22,7 +28,7 @@ namespace StringCalculatorTests
         [Test]
         public void AddNumbers_Input0_Return0()
         {
-            var actual = _calculator.AddAndParseNumber("0");
+            var actual = _calculator.Add("0");
 
             Assert.That(actual, Is.EqualTo(0));
         }
@@ -30,7 +36,7 @@ namespace StringCalculatorTests
         [Test]
         public void AddNumbers_InputOne_Return1()
         {
-            var actual = _calculator.AddAndParseNumber("1");
+            var actual = _calculator.Add("1");
 
 
             Assert.That(actual, Is.EqualTo(1));
@@ -39,7 +45,7 @@ namespace StringCalculatorTests
         [Test]
         public void AddNumbers_InputTwoNumbers_ReturnCorrectResult()
         {
-            var actual = _calculator.AddAndParseNumber("2, 2");
+            var actual = _calculator.Add("2 + 2");
 
             Assert.That(actual, Is.EqualTo(4));
         }
@@ -47,7 +53,39 @@ namespace StringCalculatorTests
         [Test]
         public void AddNumbers_DelimeterPlus_ReturnCorrectResult()
         {
-            var actual = _calculator.AddAndParseNumber("1+2");
+            var actual = _calculator.Add("1 + 2");
+
+            Assert.That(actual, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void AddNumbers_DelimeterMinus_ReturnCorrectResult()
+        {
+            var actual = _calculator.Add("5 - 2");
+
+            Assert.That(actual, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void AddNumbers_DelimeterStar_ReturnCorrectResult()
+        {
+            var actual = _calculator.Add("3 * 5");
+
+            Assert.That(actual, Is.EqualTo(15));
+        }
+
+        [Test]
+        public void AddNumbers_DelimeterSlash_ReturnCorrectResult()
+        {
+            var actual = _calculator.Add("15 / 3");
+
+            Assert.That(actual, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void AddNumbers_MoreDelimeter_ReturnCorrectResult()
+        {
+            var actual = _calculator.Add("1    +    2");
 
             Assert.That(actual, Is.EqualTo(3));
         }
